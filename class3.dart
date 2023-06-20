e88101e9f94efc7fb82f2271728e25c2661a1ea3
Hassan55jb <ranahassanraza4544@gmail.com>
2023-06-20 21:39:26 +0500

    update

--- class3.dart
+++ class3.dart
@@ -1,14 +1,7 @@
 void main() {
-  int number = 37;
-  if (number % 7 == 0 || number % 5 == 0) {
-    if (number % 7 == 0 && number % 5 == 0) {
-      print('Nuber is divisble by both');
-    } else if (number % 5 == 0) {
-      print('Number is divisable by 5');
-    } else {
-      print('Number is divisble by 7 ');
-    }
-  } else {
-    print('Number is not divided by both number');
-  }
+  List studentName = [4, 6, 3, 8, 2, 9, 1];
+  studentName.sort();
+  print(studentName);
+  List abc = List.of(studentName.reversed);
+  print(abc);
 }
